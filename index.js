5d9887c5d0395a4fa2e05da883151ff05a1491b6
Lucas Begnini <begninilucas12@gmail.com>
2022-07-04 20:33:32 -0300

    test

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const app = require("./server/config")
 
 const StartApi = async () => {
 
-    const port = process.env.PORT
+    const port = process.env.PORT || 3000
 
     try {
         app.listen(port, () => {
             console.log(`API ON => http://localhost:${port}`, )
         })
+        app.get("/", (req, res) => res.send("Hi, this is Musivyd's API"))
     } catch (error) {
         console.log("ERRO AO INICIAR A API", error)
     }
